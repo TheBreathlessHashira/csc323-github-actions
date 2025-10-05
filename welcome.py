5743ed391b8f742a494078c57829f2e45a4f0a28
Nate Jones <thebreathlesshashira@gmail.com>
2025-10-04 23:36:35 -0500

    adderg thr welcome

--- welcome.py
+++ welcome.py
@@ -1,9 +1,9 @@
 # File: welcome.py
 # 
-# Silly Python program.
-# Nate Jones
+# Sample Python program.
+# Student Name: Professor Ming
 # Class: CSC 323 Modern Technology
 
-name = input("Type your name: ")
+name=input("Type your name: ")
 print(f"Hello {name}! Welcome to GitHub Actions!")
-print("You are learning modern development workflows!")
+# all done
