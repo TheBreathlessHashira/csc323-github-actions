fc5bdd872e83d9ba118afea76641ea904d9cec39
Nate Jones <thebreathlesshashira@gmail.com>
2025-10-04 23:30:11 -0500

    Add Welcome program

---
+++ welcome.py
@@ -0,0 +1,9 @@
+# File: welcome.py
+# 
+# Silly Python program.
+# Nate Jones
+# Class: CSC 323 Modern Technology
+
+name = input("Type your name: ")
+print(f"Hello {name}! Welcome to GitHub Actions!")
+print("You are learning modern development workflows!")
